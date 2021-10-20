5bde3479a8463febe3e2ee0ae569576ea72c2c6e
Jairo Alzate <jairo.alzate@siesa.com>
2021-10-20 16:05:53 -0500

    Add change to numbers in screen

--- src/screens/CalculadoraScreen.tsx
+++ src/screens/CalculadoraScreen.tsx
@@ -1,14 +1,23 @@
-import React, {useState} from 'react';
+import React, {useRef, useState} from 'react';
 import {Text, View} from 'react-native';
 import {BotonCal} from '../components/BotonCal';
 import {styles} from '../theme/appThemes';
 
+enum Operadores {
+  sumar,
+  restar,
+  multiplicar,
+  dividir,
+}
+
 export const CalculadoraScreen = () => {
   const [numero, setNumero] = useState('0');
   const [numeroAnterior, setNumeroAnterior] = useState('0');
+  const ultimaOperacion = useRef<Operadores>();
 
   const limpiar = () => {
     setNumero('0');
+    setNumeroAnterior('0');
   };
 
   const armarNumero = (numeroTexto: string) => {
@@ -57,9 +66,40 @@ export const CalculadoraScreen = () => {
     }
   };
 
+  const cambiarNumeroPorAnterior = () => {
+    if (numero.endsWith('.')) {
+      setNumeroAnterior(numero.slice(0, -1));
+    } else {
+      setNumeroAnterior(numero);
+    }
+    setNumero('0');
+  };
+
+  const btnDividir = () => {
+    cambiarNumeroPorAnterior();
+    ultimaOperacion.current = Operadores.dividir;
+  };
+
+  const btnMultiplicar = () => {
+    cambiarNumeroPorAnterior();
+    ultimaOperacion.current = Operadores.multiplicar;
+  };
+
+  const btnSumar = () => {
+    cambiarNumeroPorAnterior();
+    ultimaOperacion.current = Operadores.sumar;
+  };
+
+  const btnRestar = () => {
+    cambiarNumeroPorAnterior();
+    ultimaOperacion.current = Operadores.restar;
+  };
+
   return (
     <View style={styles.calculadoraContainer}>
-      <Text style={styles.resultadoPequeno}>{numeroAnterior}</Text>
+      {numeroAnterior !== '0' && (
+        <Text style={styles.resultadoPequeno}>{numeroAnterior}</Text>
+      )}
       <Text style={styles.resultado} numberOfLines={1} adjustsFontSizeToFit>
         {numero}
       </Text>
@@ -67,25 +107,25 @@ export const CalculadoraScreen = () => {
         <BotonCal texto="C" color="#9B9B9B" accion={limpiar} />
         <BotonCal texto="+/-" color="#9B9B9B" accion={positivoNegativo} />
         <BotonCal texto="del" color="#9B9B9B" accion={btnDelete} />
-        <BotonCal texto="/" color="#FF9427" accion={limpiar} />
+        <BotonCal texto="/" color="#FF9427" accion={btnDividir} />
       </View>
       <View style={styles.fila}>
         <BotonCal texto="7" accion={armarNumero} />
         <BotonCal texto="8" accion={armarNumero} />
         <BotonCal texto="9" accion={armarNumero} />
-        <BotonCal texto="X" color="#FF9427" accion={limpiar} />
+        <BotonCal texto="X" color="#FF9427" accion={btnMultiplicar} />
       </View>
       <View style={styles.fila}>
         <BotonCal texto="4" accion={armarNumero} />
         <BotonCal texto="5" accion={armarNumero} />
         <BotonCal texto="6" accion={armarNumero} />
-        <BotonCal texto="-" color="#FF9427" accion={limpiar} />
+        <BotonCal texto="-" color="#FF9427" accion={btnRestar} />
       </View>
       <View style={styles.fila}>
         <BotonCal texto="1" accion={armarNumero} />
         <BotonCal texto="2" accion={armarNumero} />
         <BotonCal texto="3" accion={armarNumero} />
-        <BotonCal texto="+" color="#FF9427" accion={limpiar} />
+        <BotonCal texto="+" color="#FF9427" accion={btnSumar} />
       </View>
       <View style={styles.fila}>
         <BotonCal texto="0" accion={armarNumero} ancho />
